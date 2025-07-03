62958fb480e1a2935b29d17edac6edf7ec614161
Abhijaat <krishnaabhijaat@gmail.com>
2025-07-04 00:59:32 +0530

    fix for home route

--- app.js
+++ app.js
@@ -107,6 +107,6 @@ app.use((err, req, res, next) => {
     res.render("error.ejs", { err });
 });
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+app.listen(process.env.PORT, () => {
+    console.log("Server is running");
 });
